8773cf98da96bda2faae8afcd23c39cef349a51e
Gokhan Sari <gsari@Gokhans-MacBook-Pro.local>
2023-10-03 21:51:18 +0300

    Implemented MVC pattern

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const PORT = 3000;
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
