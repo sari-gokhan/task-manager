ece390378c11a4efe61af506dbf80e0d98e82435
Gokhan Sari <gsari@Gokhans-MacBook-Pro.local>
2023-10-05 15:45:37 +0300

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 
 // Middleware
 app.use(express.json());
@@ -13,5 +16,15 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const PORT = 3000;
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
+start();
+
+
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,12 @@
-const connectionString = 'mongodb+srv://gokhan:<password>@nodeexpressproject.limtvwh.mongodb.net/?retryWrites=true&w=majority'
+const mongoose = require('mongoose');
+
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+}
+
+module.exports = connectDB
