8280e9f3fdc14c93fd7d1649eff5e139c24e18f2
Gokhan Sari <gsari@Gokhans-MacBook-Pro.local>
2023-10-05 21:03:07 +0300

    Controllers updated

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, "Must provide a name."],
+        trim: true,
+        maxlength: [20, "Name cannot be logner than 20 characters."]
+    },
+    completed: {
+        type: Boolean, 
+        defualt: false
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
